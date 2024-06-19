0fd77e4781d58703819405c978651ad2bf673aea
Noble054 <atetteh325@st.ug.edu.gh>
2024-06-19 22:47:37 +0000

    Create GlobalStyles.js

---
+++ job/GlobalStyles.js
@@ -0,0 +1,45 @@
+/* fonts */
+export const FontFamily = {
+  medium14: "Poppins",
+  defaultBoldSubheadline: "SF Pro Text",
+  circularStd: "Circular Std",
+};
+/* font sizes */
+export const FontSize = {
+  medium16_size: 16,
+  defaultBoldSubheadline_size: 15,
+  medium12_size: 12,
+  regular13_size: 13,
+  medium14_size: 14,
+  semibold24_size: 24,
+  regular11_size: 11,
+};
+/* Colors */
+export const Color = {
+  greenPure: "#0e9d57",
+  pureWhite: "#fff",
+  labelColorLightPrimary: "#000",
+  grey70: "#afb0b6",
+  grey60: "#95969d",
+  grey95: "#f2f2f3",
+  black: "#0d0d26",
+  colorGray_100: "rgba(255, 255, 255, 0.15)",
+  bG: "#fafafd",
+  colorSteelblue: "#356899",
+  blue: "#5386e4",
+};
+/* Paddings */
+export const Padding = {
+  p_29xl: 48,
+  p_base: 16,
+  p_9xs: 4,
+};
+/* border radiuses */
+export const Border = {
+  br_81xl: 100,
+  br_xs: 12,
+  br_xl: 20,
+  br_5xl: 24,
+  br_3xs: 10,
+  br_46xl: 65,
+};
