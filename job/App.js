0c2c00516886aab1bab4def5b8906634c274c1d3
Noble054 <atetteh325@st.ug.edu.gh>
2024-06-19 22:47:52 +0000

    Create App.js

---
+++ job/App.js
@@ -0,0 +1,118 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View, Text, Pressable, TouchableOpacity } from "react-native";
+
+import LogIn from "./screens/LogIn";
+import Homepage from "./screens/Homepage";
+
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  const [hideSplashScreen, setHideSplashScreen] = React.useState(false); // Set to false to show screens
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="LogIn" component={LogIn} />
+        <Stack.Screen name="Homepage" component={Homepage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    /*<NavigationContainer>
+      {hideSplashScreen ? (
+        // Add splash screen component if required here
+        <View><Text>Splash Screen</Text></View>
+      ) : (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen
+            name="HomePage"
+            component={Homepage}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="LogIn"
+            component={LogIn}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      )}
+    </NavigationContainer>*/
+  );
+};
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const Stack = createNativeStackNavigator();
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+
+import LogIn from "./screens/LogIn";
+import Frame from "./screens/Frame";
+import Homepage from "./screens/Homepage";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View, Text, Pressable, TouchableOpacity } from "react-native";
+
+const App = () => {
+  const [hideSplashScreen, setHideSplashScreen] = React.useState(true);
+
+  return (
+    <>
+      <NavigationContainer>
+        {hideSplashScreen ? (
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen
+              name="LogIn"
+              component={LogIn}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Frame"
+              component={Frame}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Homepage"
+              component={Homepage}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        ) : null}
+      </NavigationContainer>
+    </>
+  );
+};
+export default App;*/
